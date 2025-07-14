4970127b3efc380e4715c38977742cceb6a925fb
AntonioRistevskiINKI533 <antonio.ristevski@uklo.edu.mk>
2025-07-14 22:16:50 +0200

    getCompanyTests

---
+++ LoginTest/CompanyController/GetCompanyTests.cs
@@ -0,0 +1,86 @@
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Xunit;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using StoreAPI.Services;
+using System.Net.Http;
+using StoreAPI.Models.Contexts;
+using StoreAPI.Models.Datas;
+using StoreAPI.IntegrationTests.Shared;
+using StoreAPI.Enums;
+
+public class GetCompanyIntegrationTests : IClassFixture<CustomWebApplicationFactory>
+{
+    private readonly HttpClient _client;
+    private readonly CustomWebApplicationFactory _factory;
+    private readonly HelperService _helperService;
+
+    public GetCompanyIntegrationTests(CustomWebApplicationFactory factory)
+    {
+        _factory = factory;
+        _client = factory.CreateClient();
+        _helperService = new HelperService(_factory);
+    }
+
+    [Fact]
+    public async Task GetCompany_WithValidCompanyIdAndToken_ShouldReturnOk()
+    {
+        using var scope = _factory.Services.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
+        var tokenService = scope.ServiceProvider.GetRequiredService<TokenService>();
+
+        var testUser = await _helperService.CreateTestUserAsync();
+
+        var token = tokenService.GenerateToken(testUser.Id, ((RoleEnum)testUser.RoleId).ToString());
+
+        var testCompany = await _helperService.CreateTestCompanyAsync();
+
+        var request = new HttpRequestMessage(HttpMethod.Get, $"/Company/GetCompany?companyId={testCompany.Id}");
+        request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+
+        var response = await _client.SendAsync(request);
+
+        response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+
+        var companyData = await response.Content.ReadFromJsonAsync<CompanyData>();
+        companyData.Should().NotBeNull();
+        companyData!.Id.Should().Be(testCompany.Id);
+        companyData.Name.Should().Be(testCompany.Name);
+        companyData.Address.Should().Be(testCompany.Address);
+        companyData.Phone.Should().Be(testCompany.Phone);
+
+        //Cleanup
+        context.User.Remove(testUser);
+        context.Company.Remove(testCompany);
+        await context.SaveChangesAsync();
+    }
+
+    [Fact]
+    public async Task GetCompany_WithNonExistentCompanyId_ShouldReturnNotFound()
+    {
+        using var scope = _factory.Services.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
+        var tokenService = scope.ServiceProvider.GetRequiredService<TokenService>();
+
+        var testUser = await _helperService.CreateTestUserAsync();
+
+        var token = tokenService.GenerateToken(testUser.Id, ((RoleEnum)testUser.RoleId).ToString());
+
+        var testCompany = await _helperService.CreateTestCompanyAsync();
+
+        context.Company.Remove(testCompany);
+        await context.SaveChangesAsync();
+
+        var request = new HttpRequestMessage(HttpMethod.Get, $"/Company/GetCompany?companyId={testCompany.Id}");
+        request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+
+        var response = await _client.SendAsync(request);
+
+        response.StatusCode.Should().Be(System.Net.HttpStatusCode.NotFound);
+
+        //Cleanup
+        context.User.Remove(testUser);
+        await context.SaveChangesAsync();
+    }
+}
