43c83c00dbb1a39c3199addcabb3c038064a8bcc
AntonioRistevskiINKI533 <antonio.ristevski@uklo.edu.mk>
2025-08-18 20:28:14 +0200

    appsettings and copy always. Also remove comments in repo

--- StoreAPI/Repositories/UserRepository.cs
+++ StoreAPI/Repositories/UserRepository.cs
@@ -84,40 +84,5 @@ namespace StoreAPI.Repositories
             await _context.SaveChangesAsync();
         }
 
-        //public  async Task<Book> Create(Book book)
-        //{
-        //    _context.Books.Add(book);
-        //    await _context.SaveChangesAsync();
-        //    return book;
-        //}
-        //
-        //public  async Task Delete(int id)
-        //{
-        //    var bookToDelete = await _context.Books.FindAsync(id);
-        //    _context.Books.Remove(bookToDelete);
-        //    await _context.SaveChangesAsync();
-        //}
-        //
-        //
-        //
-        //public async Task<IEnumerable<Book>> Get()
-        //{
-        //    return await _context.Books.ToListAsync();
-        //
-        //}
-        //
-        //public async Task<Book> Get(int id)
-        //{
-        //    return await _context.Books.FindAsync(id);
-        //
-        //}
-        //
-        //public async Task Update(Book book)
-        //{
-        //    _context.Entry(book).State = EntityState.Modified;
-        //    await _context.SaveChangesAsync();
-        //}
-
-
     }
 }
